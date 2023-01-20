f33a9a4cd3b98a171308636fc3e7b7a8076f094d
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-01-20 20:35:35 -0300

    Hotfix // Corrigindo a inserção do tailwind css no código.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/css/style.css'
+//Importando o css
+import './assets/css/app.css'
 
 const app = createApp(App)
 
