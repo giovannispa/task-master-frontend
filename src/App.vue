5835c04eb15505d658f5baaddab491f939f29239
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-01-24 21:45:22 -0300

    Feature // Instanciando os componentes principais de layout na app.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
+import Header from "./components/layout/Header.vue";
+import Footer from "./components/layout/Footer.vue";
+import Sidebar from "./components/layout/Sidebar.vue";
 </script>
 
 <template>
-  <div>
-    <router-view />
+  <div class="h-screen flex">
+    <Sidebar/>
+    <div class="w-full h-full">
+      <Header/>
+      <div class="w-full h-full">
+        <router-view />
+      </div>
+      <Footer/>
+    </div>
   </div>
 </template>
 
