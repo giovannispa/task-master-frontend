c7e13ede2fdac2f16786ad9188182c3395b3741f
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-01-24 21:44:08 -0300

    Feature // Componente header de layout master criado e parcialmente implementado

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,9 +1,30 @@
 <template>
-
+  <!-- Header -->
+  <div class="h-12 bg-gray-100 flex items-center justify-between shadow-sm px-5 py-2.5 w-full z-10 border-b">
+    <IconToggle class="cursor-pointer"/>
+    <Dropdown :title="{ src: '/user.png' }">
+      <DropdownItem :to="{ name: 'projetos'}">
+        <div class="flex">
+          <IconLock/> Alterar senha
+        </div>
+      </DropdownItem>
+      <DropdownItem>
+        <div class="flex">
+          <IconLogOut/> Sair
+        </div>
+      </DropdownItem>
+    </Dropdown>
+  </div>
+  <!-- End header -->
 </template>
 
 <script setup>
 
+import IconToggle from "../icons/IconToggle.vue";
+import Dropdown from "../Dropdown.vue";
+import DropdownItem from "../DropdownItem.vue";
+import IconLock from "../icons/IconLock.vue";
+import IconLogOut from "../icons/IconLogOut.vue";
 </script>
 
 <style scoped>
