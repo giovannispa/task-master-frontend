182d45579d8a5af79d2a3bbfc9058cfca0a7ba68
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-01-24 21:44:24 -0300

    Feature // Arquivo principal de useState criado e validado.

---
+++ src/composables/state.js
@@ -0,0 +1,10 @@
+import { ref } from 'vue';
+
+export function useState(initialState) {
+    const state = ref(initialState);
+    const setState = (newState) => {
+        state.value = newState;
+    };
+
+    return [state, setState];
+}
