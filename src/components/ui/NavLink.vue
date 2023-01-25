159f15c4593a603abe664ea108e6efa3c2b7490e
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-01-24 21:43:04 -0300

    Feature // Componentes dinamico de link criado e finalizado.

---
+++ src/components/ui/NavLink.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-link :to="url" class="inline-flex relative items-center w-full px-2.5 py-2.5 text-sm font-medium rounded-md border-gray-200 hover:bg-gray-300 hover:text-gray-800">
+    <slot/>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "NavLink",
+  props: {
+    url: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
