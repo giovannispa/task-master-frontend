f86d96603e22c43c9ad5aa41939a8771adbbbe61
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-02-13 21:48:47 -0300

    Feature // Criei e adicionei o componente chartjs de pie na home.

---
+++ src/components/MapaProjetos.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="d-block h-56">
+    <canvas id="myChart"></canvas>
+  </div>
+  <div class="d-block">
+    <span class="font-bold">
+      258 Projetos
+    </span>
+  </div>
+</template>
+
+<script setup>
+import Chart from 'chart.js/auto';
+import {onMounted} from "vue";
+
+onMounted(() => {
+  const ctx = document.getElementById('myChart');
+
+  const data = {
+    datasets: [{
+      label: 'My First Dataset',
+      data: [60, 5, 10, 20],
+      backgroundColor: [
+        'rgb(10, 179, 156)',
+        'rgb(64, 81, 137)',
+        'rgb(247, 184, 75)',
+        'rgb(240, 101, 72)'
+      ],
+      hoverOffset: 4
+    }],
+  };
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: data,
+  });
+});
+
+</script>
+
+<style scoped>
+
+</style>
