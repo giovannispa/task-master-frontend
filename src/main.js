fc78a07685a97b665b9faa015cf37ade8468f322
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-03-29 21:27:45 -0300

    Adicionei novos icones que são utilizados por padrão nos cards.

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import router from './router'
 
 //Importando o css
 import './assets/css/app.css'
+import IconShowMore from "./components/icons/IconShowMore.vue";
 
 const app = createApp(App)
+app.component('IconShowMore', IconShowMore);
 
 app.use(router)
 
