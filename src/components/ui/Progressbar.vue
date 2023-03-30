be958921a4164e95ae53a2348e019ba05e473d85
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-03-29 21:28:35 -0300

    Componente padrão de barra de progresso criado.

---
+++ src/components/ui/Progressbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+    <div class="bg-green-600 h-2.5 rounded-full" :style="{'width': progress + '%'}"></div>
+  </div>
+</template>
+
+<script setup>
+import {onMounted, ref,} from "vue";
+
+const props = defineProps({
+  percentage: {
+    type: Number,
+    default: 0
+  },
+});
+
+const progress = ref(0);
+
+onMounted(() => {
+  let interval = setInterval(() => {
+    if (progress.value < props.percentage) {
+      progress.value += 1;
+    } else {
+      clearInterval(interval);
+    }
+  }, 10);
+});
+
+</script>
+
+<style scoped>
+
+</style>
