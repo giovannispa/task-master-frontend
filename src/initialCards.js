67cfe077fecd780429d9cec75204fd2535a050a6
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-04-18 20:42:25 -0300

    Test // Criei um arquivo inicial com alguns ids e textos para o kanban de tarefas.

---
+++ src/initialCards.js
@@ -0,0 +1,17 @@
+export const initialCards = {
+    backlog: [
+        { id: 1, text: "Testando backlog" },
+        { id: 2, text: "Testando backlog 2" },
+        { id: 3, text: "Testando backlog 3" },
+    ],
+    dev: [
+        { id: 4, text: "Testando dev" },
+        { id: 5, text: "Testando dev 2" },
+    ],
+    test: [
+        { id: 6, text: "Testando test" },
+    ],
+    conclusion: [
+        { id: 7, text: "Testando conslusion" },
+    ]
+}
