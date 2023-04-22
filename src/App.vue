713cdf8419140ced807eb3d44d874fbe1b82e6c1
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-04-21 22:13:45 -0300

    Feature // Criei um componente chamado Default que esta armazenando o layout padrão do sistema.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <template>
-  <div class="flex min-h-screen w-full font-sans bg-gray-300">
-    <Sidebar/>
-    <main class="flex min-h-screen flex-1 flex-col">
-      <Header/>
-      <div class="w-full">
-        <div class="w-full min-h-screen">
-          <router-view />
-        </div>
-      </div>
-      <Footer/>
-    </main>
-  </div>
+  <Default/>
 </template>
 
 <script setup>
-import Header from "./components/layout/Header.vue";
-import Footer from "./components/layout/Footer.vue";
-import Sidebar from "./components/layout/Sidebar.vue";
 
+import Default from "./components/layout/Default.vue";
 </script>
 
 <style scoped>
---
+++ src/components/layout/Default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex min-h-screen w-full font-sans bg-gray-300">
+    <Sidebar/>
+    <main class="flex min-h-screen flex-1 flex-col">
+      <Header/>
+      <div class="w-full">
+        <div class="w-full min-h-screen">
+          <router-view />
+        </div>
+      </div>
+      <Footer/>
+    </main>
+  </div>
+</template>
+
+<script setup>
+
+import Sidebar from "./Sidebar.vue";
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+</script>
+
+<style scoped>
+
+</style>
