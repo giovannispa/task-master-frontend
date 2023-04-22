0c9ec3fb778129c6ff6e0396d82faa2f1c1bb843
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-04-21 22:17:59 -0300

    Feature // Lazyload implementado nas rotas do sistema

--- src/router/index.js
+++ src/router/index.js
@@ -6,38 +6,43 @@ import EquipesView from '../views/EquipesView.vue'
 import RelatoriosView from '../views/RelatoriosView.vue'
 import UsuariosView from '../views/UsuariosView.vue'
 
+function lazyLoad(view) {
+  return () => import(`../views/${view}.vue`);
+}
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: lazyLoad("HomeView")
     },
     {
       path: '/projetos',
       name: 'projetos',
-      component: ProjetosView
+      component: lazyLoad("ProjetosView")
     },
     {
       path: '/tarefas',
       name: 'tarefas',
-      component: TarefasView
+      component: lazyLoad("TarefasView")
     },
     {
       path: '/equipes',
       name: 'equipes',
-      component: EquipesView
+      component: lazyLoad("EquipesView")
     },
     {
       path: '/relatorios',
       name: 'relatorios',
-      component: RelatoriosView
+      component: lazyLoad("RelatoriosView")
     },
     {
       path: '/usuarios',
       name: 'usuarios',
-      component: UsuariosView
+      component: lazyLoad("UsuariosView")
     },
   ]
 })
