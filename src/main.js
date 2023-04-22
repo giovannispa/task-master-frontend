c80fb584bced2327f186b38c90a45467b452238b
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-04-21 22:14:48 -0300

    Feature // Defini o layout default e login como globais.

--- src/main.js
+++ src/main.js
@@ -5,9 +5,13 @@ import router from './router'
 //Importando o css
 import './assets/css/app.css'
 import IconShowMore from "./components/icons/IconShowMore.vue";
+import Default from "./components/layout/Default.vue";
+import Login from "./components/layout/Login.vue";
 
 const app = createApp(App)
 app.component('IconShowMore', IconShowMore);
+app.component('default-layout', Default);
+app.component('login-layout', Login);
 
 app.use(router)
 
