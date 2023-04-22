becebe78cd11aefc0e06581bd39a89ef1e2e92b8
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-04-21 22:14:17 -0300

    Feature // Criei inicialmente os componentes de login e layout de login, mas não implementei nada ainda.

---
+++ src/components/layout/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <template>
+    <div class="flex justify-center items-center min-h-screen">
+      <router-view />
+    </div>
+  </template>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex justify-center items-center h-screen bg-[#607D8B]">
+    <div>
+      <h1>Login</h1>
+      <hr>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'login',
+  // outros códigos do componente Login.vue
+}
+</script>
+
+<style scoped>
+
+</style>
